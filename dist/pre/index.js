fd9cf5ae4bbd92bf6154506f62ab1b7d56f25762
prateek-stepsecurity <prateek@stepsecurity.io>
2026-05-26 21:55:16 +0530

    upgrade agent version to 1.8.7

--- dist/pre/index.js
+++ dist/pre/index.js
@@ -85622,7 +85622,7 @@ function installAgent(isTLS, configStr) {
             encoding: "utf8",
         });
         if (isTLS) {
-            downloadPath = yield tool_cache.downloadTool(`https://github.com/step-security/agent-ebpf/releases/download/v1.8.4/harden-runner_1.8.4_linux_${variant}.tar.gz`, undefined, auth);
+            downloadPath = yield tool_cache.downloadTool(`https://github.com/step-security/agent-ebpf/releases/download/v1.8.7/harden-runner_1.8.7_linux_${variant}.tar.gz`, undefined, auth);
         }
         else {
             if (variant === "arm64") {
@@ -85657,7 +85657,7 @@ function installAgentBravo(configStr) {
         const token = lib_core.getInput("token", { required: true });
         const auth = `token ${token}`;
         const variant = process.arch === "x64" ? "amd64" : "arm64";
-        const downloadPath = yield tool_cache.downloadTool(`https://github.com/step-security/agent-ebpf/releases/download/v1.8.6/harden-runner-bravo_1.8.6_linux_${variant}.tar.gz`, undefined, auth);
+        const downloadPath = yield tool_cache.downloadTool(`https://github.com/step-security/agent-ebpf/releases/download/v1.8.7/harden-runner-bravo_1.8.7_linux_${variant}.tar.gz`, undefined, auth);
         // if (!verifyChecksum(downloadPath, true, variant, "linux", "bravo")) {
         //   return false;
         // }
