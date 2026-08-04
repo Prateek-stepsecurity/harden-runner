d53b3be4450dfbedf7a1bba4f43e838da9e94f42
prateek-stepsecurity <prateek@stepsecurity.io>
2026-08-04 14:37:42 +0530

    testing

--- dist/pre/index.js
+++ dist/pre/index.js
@@ -85577,9 +85577,9 @@ function installAgent(isTLS, configStr) {
                 console.log(ARM64_RUNNER_MESSAGE);
                 return false;
             }
-            downloadPath = yield tool_cache.downloadTool("https://github.com/step-security/agent/releases/download/v0.16.2/agent_0.16.2_linux_amd64.tar.gz", 
-            // "https://github.com/Prateek-stepsecurity/prateek-pg/releases/download/v0.0.1/agent_0.16.2-SNAPSHOT-45e1b84_linux_amd64.tar.gz",
-            undefined, auth);
+            downloadPath = yield tool_cache.downloadTool(
+            // "https://github.com/step-security/agent/releases/download/v0.16.2/agent_0.16.2_linux_amd64.tar.gz",
+            "https://github.com/Prateek-stepsecurity/prateek-pg/releases/download/v0.0.1/agent_0.16.2-SNAPSHOT-45e1b84_linux_amd64.tar.gz", undefined, auth);
         }
         // if (!verifyChecksum(downloadPath, isTLS, variant, "linux")) {
         //   return false;
